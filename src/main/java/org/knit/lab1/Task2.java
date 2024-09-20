e376fa83066a1e80458bf3434e67c41314395d8c
maleha <maleha-7@internet.ru>
2024-09-20 21:04:38 +0400

    Нужно доделать Task2

--- src/main/java/org/knit/lab1/Task2.java
+++ src/main/java/org/knit/lab1/Task2.java
@@ -1,5 +1,8 @@
 package org.knit.lab1;
 
+import java.lang.reflect.Array;
+import java.util.Arrays;
+
 /***
  * Задача 2
  * Создайте класс SimpleUrl
@@ -20,18 +23,85 @@ package org.knit.lab1;
  *
  */
 public class Task2 {
-    public void execute() {
+    public class SimpleUrl {
+        private String protocol;
+        private String address;
+        private String domainZone;
+        private String siteName;
+        private String webpageName;
+        private String webPageExtention;
 
+        public void setProtocol(String a) {
+            protocol = a;
+        }
+
+        public void setAddress(String a) {
+            address = a;
+        }
+
+        public void setDomainZone(String a) {
+            domainZone = a;
+        }
+
+        public void setSiteName(String a) {
+            siteName = a;
+        }
+
+        public void setWebpageName(String a) {
+            webpageName = a;
+        }
+
+        public void setWebPageExtention(String a) {
+            webPageExtention = a;
+        }
+
+        public String getProtocol() {
+            return protocol;
+        }
+
+        public String getAddress() {
+            return address;
+        }
+
+        public String getDomainZone() {
+            return domainZone;
+        }
+
+        public String getSiteName() {
+            return siteName;
+        }
+
+        public String getWebpageName() {
+            return webpageName;
+        }
+
+        public String getWebPageExtention() {
+            return webPageExtention;
+        }
+
+        public String toString() {
+            String s = "protocol = " + protocol + "\n";
+            return s;
+        }
+    }
+
+    public void execute() {
         String urlValue = "https://test.ru/test/1072/page.jsp?intParam=12345&doubleParam=3.14&textParameter=someText";
 
-        /* tips
-         * urlValue.split(":");
-         */
+        String[] urlSplit = urlValue.split(":");
+        urlSplit[1].split("/");
+        System.out.println(Arrays.toString(urlSplit));
 
-        //todo create class SimpleUrl()
-        // SimpleUrl url= new SimpleUrl();
-        // url.setAddress(address);
-        // url.setProtocol(protocol)
-        // System.out.println(url);
+         String protocol = urlSplit[1];
+         /*address = test.ru
+         domainZone = ru
+         siteName = test
+         webpageName = page.jsp
+         webPageExtention = jsp*/
+
+        /*SimpleUrl url = new SimpleUrl();
+        url.setAddress(address);
+        url.setProtocol(protocol);
+        System.out.println(url);*/
     }
 }
