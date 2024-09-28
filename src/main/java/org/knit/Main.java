7bcef37ab2c671d1f1e4af1edb4170f3fbbf75c2
maleha <maleha-7@internet.ru>
2024-09-28 09:56:29 +0400

    init tasks/Task6

--- src/main/java/org/knit/Main.java
+++ src/main/java/org/knit/Main.java
@@ -1,4 +1,10 @@
 package org.knit;
 
+import org.knit.lab3.Task6;
+
 public class Main {
+    public static void main(String[] args) {
+        Task6 task6 = new Task6();
+        task6.execute();
+    }
 }
---
+++ src/main/java/org/knit/lab3/Guard.java
@@ -0,0 +1,11 @@
+package org.knit.lab3;
+
+public class Guard extends Warrior {
+    int maxHealth = 600;
+    int damage = 50;
+    int armor = 0;
+
+    public Guard(String name) {
+        super(name);
+    }
+}
