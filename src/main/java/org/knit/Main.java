305fec0d24c81c2dd021b2de18b084de6f0a3d10
maleha <maleha-7@internet.ru>
2024-10-04 14:12:13 +0400

    hierarchy upd

--- src/main/java/org/knit/Main.java
+++ src/main/java/org/knit/Main.java
@@ -1,9 +1,24 @@
 package org.knit;
 
+//import org.knit.lab1.Task1;
+//import org.knit.lab1.Task2;
+//import org.knit.lab2.Task3;
+//import org.knit.lab2.Task4;
+//import org.knit.lab2.Task5;
 import org.knit.lab3.Task6;
 
 public class Main {
     public static void main(String[] args) {
+//        Task1 task1 = new Task1();
+//        task1.execute();
+//        Task2 task2 = new Task2();
+//        task2.execute();
+//        Task3 task3 = new Task3();
+//        task3.execute();
+//        Task4 task4 = new Task4();
+//        task4.execute();
+//        Task5 task5 = new Task5();
+//        task5.execute();
         Task6 task6 = new Task6();
         task6.execute();
     }
