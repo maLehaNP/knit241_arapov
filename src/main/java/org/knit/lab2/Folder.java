5d2e619eb586e19059fd4ba171d60cf2daa2d1aa
maleha <maleha-7@internet.ru>
2024-10-04 14:35:54 +0400

    Reformat.

---
+++ src/main/java/org/knit/lab2/Folder.java
@@ -0,0 +1,29 @@
+package org.knit.lab2;
+
+public class Folder extends FileSystemComponent {
+
+    @Override
+    public String getName() {
+        return "";
+    }
+
+    @Override
+    public byte getSize() {
+        return 0;
+    }
+
+    @Override
+    public void add(FileSystemComponent component) {
+
+    }
+
+    @Override
+    public void remove(FileSystemComponent component) {
+
+    }
+
+    @Override
+    public void display(String indent) {
+
+    }
+}
--- src/main/java/org/knit/lab2/Task5.java
+++ src/main/java/org/knit/lab2/Task5.java
@@ -1,73 +1,7 @@
 package org.knit.lab2;
 
 public class Task5 {
-    public static abstract class FileSystemComponent {
-        public abstract String getName();
+    public void execute() {
 
-        public abstract byte getSize();
-
-        public abstract void add(FileSystemComponent component);
-
-        public abstract void remove(FileSystemComponent component);
-
-        public abstract void display(String indent);
-    }
-
-    public static class File extends FileSystemComponent {
-        String name;
-        byte size;
-
-        @Override
-        public String getName() {
-            return name;
-        }
-
-        @Override
-        public byte getSize() {
-            return size;
-        }
-
-        @Override
-        public void add(FileSystemComponent component) {
-
-        }
-
-        @Override
-        public void remove(FileSystemComponent component) {
-
-        }
-
-        @Override
-        public void display(String indent) {
-
-        }
-    }
-
-    public static class Folder extends FileSystemComponent {
-
-        @Override
-        public String getName() {
-            return "";
-        }
-
-        @Override
-        public byte getSize() {
-            return 0;
-        }
-
-        @Override
-        public void add(FileSystemComponent component) {
-
-        }
-
-        @Override
-        public void remove(FileSystemComponent component) {
-
-        }
-
-        @Override
-        public void display(String indent) {
-
-        }
     }
 }
