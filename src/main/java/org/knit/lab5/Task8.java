82863e1df1bdb51a11427b801edbe6b6cff9d9a6
maleha <maleha-7@internet.ru>
2024-10-26 09:10:55 +0400

    Task8: nothing changed.

--- src/main/java/org/knit/lab5/Task8.java
+++ src/main/java/org/knit/lab5/Task8.java
@@ -11,7 +11,7 @@ public class Task8 {
         float[] prices = {1.2342f, 2.2341f, 3.234123f, 4.2341f};
         boolean[] packages = {true, false};
         List<ShopItem> testData = new ArrayList<ShopItem>();
-        int n_samples = 10;
+        int n_samples = 100;
         HashSet<ShopItem> idenItems = new HashSet<ShopItem>();
 
         for (int i = 0; i < n_samples; i++) {
@@ -22,17 +22,20 @@ public class Task8 {
         }
         testData.sort(itemComp);
         int idenItemCount = 0;
+        int sumIdenItemCount = 0;
         for (int i = 0; i < n_samples; i++) {
             System.out.println(testData.get(i).toString());
-            if (!idenItems.contains(testData.get(i))) {
-                for (int j = i + 1; j < n_samples; j++) {
-                    if (testData.get(i).equals(testData.get(j))) {
-                        idenItemCount++;
-                    }
-                    idenItems.add(testData.get(i));
+            if (idenItems.contains(testData.get(i))) {
+                continue;
+            } else idenItems.add(testData.get(i));
+            idenItemCount = 0;
+            for (int j = i + 1; j < n_samples; j++) {
+                if (testData.get(i).equals(testData.get(j))) {
+                    idenItemCount++;
                 }
             }
+            sumIdenItemCount+=idenItemCount;
         }
-        System.out.println("Количество одинаковых ShopItem: " + idenItemCount);
+        System.out.println("Количество одинаковых ShopItem: " + sumIdenItemCount);
     }
 }
