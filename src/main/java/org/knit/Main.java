5da38e01162e48ff9b77ee5041a0542b691f8173
maleha <maleha-7@internet.ru>
2024-11-15 23:27:58 +0400

    Task Main imports.

--- src/main/java/org/knit/Main.java
+++ src/main/java/org/knit/Main.java
@@ -1,12 +1,11 @@
 package org.knit;
 
-import org.knit.lab1.Task1;
-import org.knit.lab1.Task2;
-import org.knit.lab2.Task3;
-import org.knit.lab2.Task4;
-import org.knit.lab3.Task6;
-import org.knit.lab5.Task8;
-
+//import org.knit.lab1.Task1;
+//import org.knit.lab1.Task2;
+//import org.knit.lab2.Task3;
+//import org.knit.lab2.Task4;
+//import org.knit.lab3.Task6;
+//import org.knit.lab5.Task8;
 
 public class Main {
     public static void main(String[] args) {
