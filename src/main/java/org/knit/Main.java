1e2c16f542ad32b21fb6b2aac758220dab25b038
maleha <maleha-7@internet.ru>
2024-11-15 23:27:05 +0400

    Task exemplars.

--- src/main/java/org/knit/Main.java
+++ src/main/java/org/knit/Main.java
@@ -10,12 +10,12 @@ import org.knit.lab5.Task8;
 
 public class Main {
     public static void main(String[] args) {
-        Task1 task1 = new Task1();
-        Task2 task2 = new Task2();
-        Task3 task3 = new Task3();
-        Task4 task4 = new Task4();
-        Task6 task6 = new Task6();
-        Task8 task8 = new Task8();
+//        Task1 task1 = new Task1();
+//        Task2 task2 = new Task2();
+//        Task3 task3 = new Task3();
+//        Task4 task4 = new Task4();
+//        Task6 task6 = new Task6();
+//        Task8 task8 = new Task8();
 
 //        task1.execute();
 //        task2.execute();
