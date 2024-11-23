d0bc29146f728cda944b81f49ad4e35f39911588
maleha <maleha-7@internet.ru>
2024-11-23 08:55:06 +0400

    //s

--- src/main/java/org/knit/Main.java
+++ src/main/java/org/knit/Main.java
@@ -1,24 +1,24 @@
 package org.knit;
 
-import org.knit.lab1.Task1;
-import org.knit.lab1.Task2;
-import org.knit.lab2.Task3;
-import org.knit.lab2.Task4;
-import org.knit.lab2.Task5;
-import org.knit.lab3.Task6;
-import org.knit.lab5.Task8;
-import org.knit.lab9.Task15;
+//import org.knit.lab1.Task1;
+//import org.knit.lab1.Task2;
+//import org.knit.lab2.Task3;
+//import org.knit.lab2.Task4;
+//import org.knit.lab2.Task5;
+//import org.knit.lab3.Task6;
+//import org.knit.lab5.Task8;
+//import org.knit.lab9.Task15;
 
 public class Main {
     public static void main(String[] args) {
-        Task1 task1 = new Task1();
-        Task2 task2 = new Task2();
-        Task3 task3 = new Task3();
-        Task4 task4 = new Task4();
-        Task5 task5 = new Task5();
-        Task6 task6 = new Task6();
-        Task8 task8 = new Task8();
-        Task15 task15 = new Task15();
+//        Task1 task1 = new Task1();
+//        Task2 task2 = new Task2();
+//        Task3 task3 = new Task3();
+//        Task4 task4 = new Task4();
+//        Task5 task5 = new Task5();
+//        Task6 task6 = new Task6();
+//        Task8 task8 = new Task8();
+//        Task15 task15 = new Task15();
 
 //        task1.execute();
 //        task2.execute();
@@ -27,6 +27,6 @@ public class Main {
 //        task5.execute();
 //        task6.execute();
 //        task8.execute();
-        task15.execute();
+//        task15.execute();
     }
 }
