d752096233b5b2147419ee522f4b7754c90d5879
maleha <maleha@DESKTOP-TV89Q3E>
2025-02-22 12:33:47 +0400

    Main.java

--- src/main/java/org/knit/Main.java
+++ src/main/java/org/knit/Main.java
@@ -22,7 +22,7 @@ package org.knit;
 //import org.knit.lab2_1.Task2_1;
 //import org.knit.lab2_1.Task2_2;
 //import org.knit.lab2_1.Task2_3;
-import org.knit.lab2_2.Task2_4;
+//import org.knit.lab2_2.Task2_4;
 
 public class Main {
     public static void main(String[] args) {
@@ -48,6 +48,6 @@ public class Main {
 //        Task2_1.execute();
 //        Task2_2.execute();
 //        Task2_3.execute();
-        Task2_4.execute();
+//        Task2_4.execute();
     }
 }
