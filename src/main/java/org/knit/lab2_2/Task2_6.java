a1a1478e815306f649fe307ef96833a338f114a8
maleha <maleha@DESKTOP-TV89Q3E>
2025-03-01 13:19:20 +0400

    init

---
+++ src/main/java/org/knit/lab2_2/Task2_6.java
@@ -0,0 +1,40 @@
+package org.knit.lab2_2;
+
+public class Task2_6 {
+    public static void execute() {
+        Crossing crossing = new Crossing();
+        final int carCount = 20;
+        final int trainCount = 3;
+
+        /*for (int i = 0; i < carCount; i++) {
+            int carNum = i;
+            new Thread(() -> {
+                crossing.car(carNum);
+            }).start();
+        }*/
+
+        Thread threadCars = new Thread(() -> {
+            for (int i = 1; i <= carCount; i++) {
+                crossing.car(i);
+                try {
+                    Thread.sleep(200);
+                } catch (InterruptedException e) {
+                    throw new RuntimeException(e);
+                }
+            }
+        });
+
+        Thread threadTrains = new Thread(() -> {
+            for (int i = 1; i <= trainCount; i++) {
+                crossing.train(i);
+                try {
+                    Thread.sleep(4000);
+                } catch (InterruptedException e) {
+                    throw new RuntimeException(e);
+                }
+            }
+        });
+        threadCars.start();
+        threadTrains.start();
+    }
+}
