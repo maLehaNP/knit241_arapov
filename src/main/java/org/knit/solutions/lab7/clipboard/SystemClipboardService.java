294119b6099ad7255a930c15b9a28bff035f4f90
maleha <maleha@DESKTOP-TV89Q3E>
2025-05-17 09:43:25 +0400

    Make clipboard cleaning and fix pom

--- src/main/java/org/knit/solutions/lab7/clipboard/SystemClipboardService.java
+++ src/main/java/org/knit/solutions/lab7/clipboard/SystemClipboardService.java
@@ -4,12 +4,21 @@ import org.springframework.stereotype.Service;
 
 import java.awt.*;
 import java.awt.datatransfer.StringSelection;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+
+import static java.util.concurrent.TimeUnit.SECONDS;
 
 @Service
 public class SystemClipboardService implements ClipboardService {
+    private final ScheduledExecutorService scheduler = Executors.newScheduledThreadPool(1);
+    Runnable clipboardCleaner = () -> Toolkit.getDefaultToolkit().getSystemClipboard().setContents(new StringSelection(""), null);  // Очистка буфера
+
     public void copyToClipboard(String password) {
         Toolkit.getDefaultToolkit()
                 .getSystemClipboard()
                 .setContents(new StringSelection(password), null);
+
+        scheduler.schedule(clipboardCleaner, 30, SECONDS);  // Автоматическая очистка буфера через 30 с
     }
 }
